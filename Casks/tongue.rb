6c6041d3f32fd578155eab63bb57a8d5ae170481
Chad Newbry <chad.newbry@gmail.com>
2026-03-28 22:59:17 -0400

    Update Tongue cask to 0.4.0

--- Casks/tongue.rb
+++ Casks/tongue.rb
@@ -1,6 +1,6 @@
 cask "tongue" do
-  version "0.1.0"
-  sha256 "0b5c395b9db699ff23b73116ac70ef875f9df94ae8f3598a55f70007e0948c9d"
+  version "0.4.0"
+  sha256 "17c573c7aec5c17d8fa14f856338c16fbf62da4ddb279fc95e831d01a8fda9ca"
 
   url "https://trytongue.com/downloads/Tongue-#{version}.dmg"
   name "Tongue"
